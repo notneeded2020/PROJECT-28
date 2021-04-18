49cd94c4a858a6f222d7f61b7aef3af73a81f08a
notneeded2020 <75374552+notneeded2020@users.noreply.github.com>
2021-04-18 18:30:22 +0530

    Update Stone.js

--- js/Stone.js
+++ js/Stone.js
@@ -1,7 +1,7 @@
 class Stone {
   constructor (x,y,radius){
     var options= {
-      isStatic: true,
+      isStatic: false,
       restitution: 0,
       friction:1,
       density:1.2
@@ -10,7 +10,7 @@ class Stone {
      this.x      = x
      this.y      = y  
      this.image  = loadImage("images/stone.png");
-     this.body   = Bodies.circle(this.x, this.y, this.radius, options);
+     this.body   = Bodies.circle(this.x, this.y, this.radius/2, options);
      World.add(world,this.body);
 
   }
